ecb1bdcca91a07936f7f85e407767d81e3199356
kdagnes <karenmargaret02@gmail.com>
2017-02-16 23:17:35 +0800

    Add files via upload

---
+++ 01-Project-Documents/Codes/logout.php
@@ -0,0 +1,28 @@
+<!--  Code License
+Author: Matthew Aycocho
+This is a course requirement for CS 192 Software Engineering II under the
+supervision of Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer
+Science, College of Engineering, University of the Philippines, Diliman for
+the AY 2016-2017.
+-->
+<!--  Code History
+Name of Programmer; Change Date; Change Description
+Matthew Aycocho; Feb. 16, 2017; Initial code
+-->
+<!--
+File creation date: Feb. 16, 2017
+Development Group: The A-Team
+Purpose: destroy session of user
+-->
+<?php
+session_start();
+$_SESSION = array();
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+session_destroy();
+?>
